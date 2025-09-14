a63b70241822505fa1f882c28519227f1e2db1c9
kolay <cats@feel.so>
2025-09-14 20:36:51 +0300

    [~] fix gid

--- up.sh
+++ up.sh
@@ -4,7 +4,7 @@
 # Get uid and gid of www/
 # Run the containers
 if [ -f .env ]; then
-    WWW_UID=$(stat -c "%u" ./www) WWW_GID=$(stat -c "%d" ./www) docker compose up -d
+    WWW_UID=$(stat -c "%u" ./www) WWW_GID=$(stat -c "%g" ./www) docker compose up -d
 else
     echo ".env file not found in current directory. please copy and edit .env.example"
     exit 1
